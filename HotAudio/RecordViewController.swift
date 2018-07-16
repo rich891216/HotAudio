68d215bf90fa02f234cafafa19be426bb28a71b6
richard891216 <41025296+rich891216@users.noreply.github.com>
2018-07-16 11:21:24 -0700

    Initial Commit
    First Commit. Set up basic UI.

---
+++ HotAudio/RecordViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RecordViewController.swift
+//  HotAudio
+//
+//  Created by Richard Wang on 7/16/18.
+//  Copyright © 2018 Richard Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecordViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
