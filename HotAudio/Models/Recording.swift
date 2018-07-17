28bee83cd85a0d3e7b44b2bcaa23f4ad84e8d4f5
richard891216 <41025296+rich891216@users.noreply.github.com>
2018-07-17 15:35:26 -0700

    calculation

--- HotAudio/Models/Recording.swift
+++ HotAudio/Models/Recording.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 class Recording {
+    var attractivePercentage : Int
+    init(attractivePercentage : Int) {
+        self.attractivePercentage = attractivePercentage
+    }
+    func calculateAttractiveness(hertz: Double) -> Int {
+        if hertz == 96.00 {
+            
+            attractivePercentage = 100
+        }
+        if hertz > 96.00 {
+            attractivePercentage = Int ((hertz - 96) / 84 * 100)
+        }
+        if hertz < 96.00 {
+            attractivePercentage = Int ((96 - hertz) / 36 * 100)
+        }
+        return attractivePercentage
+    }
     
 }
