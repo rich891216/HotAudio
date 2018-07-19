6045a6009455dac9b15dde07c6ab2d87ef7749d3
Ronald Chan <ronald.chan850@gmail.com>
2018-07-19 12:57:11 -0700

    added responses

--- HotAudio/ResultViewController.swift
+++ HotAudio/ResultViewController.swift
@@ -16,14 +16,37 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     weak var delegate:ResultViewControllerDelegate?
     var recording:Recording?
+    let responses:[String]=["Wow your voice is beautiful",
+                            "Very nice but Morgan Freeman exists",
+                            "Sounds like a regular human being",
+                            "Have you ever thought about talking through a voice changer?",
+                            "Can you stop"]
     var averageFrequency:Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getAverage()
         print(averageFrequency)
-        percentageLabel!.text = "\(Recording.calculateAttractiveness(hertz: averageFrequency))%"
-
+        var score=Recording.calculateAttractiveness(hertz: averageFrequency)
+        percentageLabel!.text = "\(score)%"
+        var response:String
+        if (score>80) {
+            response=responses[0]
+        }
+        else if (score>60) {
+            response=responses[1]
+        }
+        else if (score>40) {
+            response=responses[2]
+        }
+        else if (score>20) {
+            response=responses[3]
+        }
+        else {
+            response=responses[4]
+        }
+        resultLabel.text=response
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
